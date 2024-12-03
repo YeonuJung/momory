4fe905fcac756dd3be2cfb80fec8ec61050adc16
YeonuJung <gsh95214@naver.com>
2024-12-04 02:37:42 +0900

    헤더 화살표 크기 조정

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -8,7 +8,7 @@ export default function Header(props: HeaderProps) {
   return (
     <header className="absolute top-[6.67vw] flex w-full items-center justify-between px-[5.83vw] xs:top-[3.2rem] xs:px-[2.8rem]">
       {
-        <div className="flex h-[4.62vw] w-[7.1vw] cursor-pointer items-center justify-center xs:h-[1.9rem] xs:w-[3.408rem]">
+        <div className="flex h-[4.62vw] w-[7.1vw] cursor-pointer items-center justify-center xs:h-[2.219rem] xs:w-[3.408rem]">
           <Image
             src="/image/arrow-left.png"
             width={14.12}
